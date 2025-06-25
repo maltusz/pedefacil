be212249aabb681cb28d13fe6931d4b7d933b7e0
Davi Maltus <168310043+maltusz@users.noreply.github.com>
2025-06-25 10:18:49 -0300

    Update settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['31.97.174.51']
+ALLOWED_HOSTS = ['31.97.174.51', 'pedefacil.app']
 
 
 # Application definition
